d1b4a7e4baaf1df17b7963e5c8288085befd0142
HnpawsPham <hanpham123th@gmail.com>
2024-10-16 23:13:51 +0700

    v67

---
+++ HSG_competition/High school (THPT)/Bit/CHIA-TAO.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define el "\n"
+
+int n;
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin>>n;
+    ll a[n];
+    for(int i = 0; i < n;i++) cin>>a[i];
+
+    ll sum1 = 0, sum2 = 0, res = LLONG_MAX;
+    
+    for(int i = 0; i < (1 << n); i++){
+        for(int j = 0; j < n; j++){
+            if((i >> j) &1) sum1 += a[j];
+            else sum2 += a[j];
+        }
+        res = min(abs(sum1 - sum2), res);
+        sum1 = sum2 = 0;
+    }
+    cout<<res<<el;
+
+    return 0;
+}
--- HSG_competition/High school (THPT)/Bit/SINH-TAP-CON.cpp
+++ HSG_competition/High school (THPT)/Bit/SINH-TAP-CON.cpp
@@ -7,12 +7,12 @@ int main(){
     cin.tie(0);
     cout.tie(0);
 
-    int n = 6;
-    int a[] = {3, 6, 3, 2, 8, 4};
+    int n = 5;
+    int a[] = {3, 6, 2, 8, 4};
 
     for(int i = 0; i < (1 << n); i++){
-        for(int j = 0;j < 4;j ++){
-            if(i & (1 << j)) cout<<a[j]<<" ";
+        for(int j = 0;j < n;j++){
+            if((i >> j) & 1) cout<<a[j]<<" ";
         }
         cout<<el;
     }
