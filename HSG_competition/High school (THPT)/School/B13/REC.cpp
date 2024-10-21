8ca61bd51f88ff16840ca3e3cf11accd5dfa8aee
HnpawsPham <hanpham123th@gmail.com>
2024-10-21 23:22:01 +0700

    v70

--- HSG_competition/High school (THPT)/School/B13/REC.cpp
+++ HSG_competition/High school (THPT)/School/B13/REC.cpp
@@ -6,14 +6,25 @@ int n, m, t, res = 0;
 const int maxn = 1005;
 int a[maxn][maxn];
 
-void solve (int h, int w, int x){
-    for(int i = 1; i <= h;i++){
-        for(int j = 1; j <=w; j++){
-            if (abs(x - a[i][j]) > 1) return;
-            res = max(res, i * j);
+int solve (int h[]){
+    int maxx = 0;
+    stack<int> s;
+
+    for(int i = 1; i <= m; i++){
+        while(!s.empty() && h[s.top()] > h[i]){
+            int h1 = h[s.top()]; s.pop();
+            int w = (s.empty() ? i : i - s.top() - 1);
+            maxx = max(maxx, h1 * w);
         }
+        s.push(i);
     }
-    return;
+    while (!s.empty()) {
+        int h1 = h[s.top()];
+        s.pop();
+        int w = (s.empty() ? m : m - s.top());
+        maxx = max(maxx, h1 * w);
+    }
+    return maxx;
 }
 
 int main(){
@@ -28,29 +39,31 @@ int main(){
         for(int j = 1; j<=m;j++) cin>>a[i][j];
 
     int h01[n + 1][m + 1], h12[n + 1][m + 1];
-    for(int j = 0; j <=m;j++)
-        h01[0][j] = h12[0][j] = 0;
 
-    for(int i = 1; i<=n;i++)
-        for(int j = 1; j <=m; j++){
-            h01[i][j] = h12[i][j] = 0;
+    for(int i = 0;i<=n;i++)
+        for(int j = 0;j<=m;j++) h01[i][j] = h12[i][j] = 0;
+    
+
+    for(int i = 1;i<=n;i++)
+        for(int j = 1; j<=m; j++){
             if(a[i][j] < 2) h01[i][j] = h01[i - 1][j] + 1;
             if(a[i][j] > 0) h12[i][j] = h12[i - 1][j] + 1;
         }
-
     cin>>t;
     while(t--){
         int u, d;
         cin>>u>>d;
 
-        for(int i = u; i <= d;i++){
-            for(int j = 1; j <=m;j++){
-                solve(i, j, a[i][j]);
-            }
-        }
-        cout<<res<<el;
-        res = 0;
-    }
+        int l01[m + 1], l12[m + 1];
 
+        for(int j = 1; j <= m;j++){
+            l01[j] = h01[d][j] - h01[u - 1][j];
+            l12[j] = h12[d][j] - h12[u - 1][j];
+        }  
+        
+        int s1 = solve(l01);
+        int s2 = solve(l12);
+        cout<<max(s1, s2)<<el;
+    }
     return 0;
 }
