70d37a0062966cb4521b6ee91ae8bd8bf8d3517f
HnpawsPham <hanpham123th@gmail.com>
2024-11-09 23:25:26 +0700

    v84

--- HSG_competition/High school (THPT)/School/B17/WATER.cpp
+++ HSG_competition/High school (THPT)/School/B17/WATER.cpp
@@ -21,31 +21,27 @@ int main(){
     for(int i = 1;i<=n;i++) a[i][0] = a[i][1];
 
     int sum = 0;
-    for(int j = 1;j<=m;j++){
+    for(int j = 1;j<m;j++){
         for(int i=1;i<n;i++){
             int t = max(a[i + 1][j], a[i][j]) - min(a[i][j], a[i + 1][j]);
+            int d = max(a[i - 1][j], a[i][j]) - min(a[i-1][j], a[i][j]);
+            t = max(d, t);
             cout<<t<<" ";
             sum += t;
-            du[i + 1][j] = t;
+            du[i][j] = t;
         }
         cout<<el;
     }
-    cout<<el;
+    // cout<<el;
 
-    for(int i = 1;i<=n;i++){
-        for(int j = 1; j < m;j++){
-            int t = max(a[i][j], a[i][j + 1]) - min(a[i][j], a[i][j + 1]);
-            cout<<t<<" ";
-            if(!du[i][j + 1]) sum += t;
-        }
-        cout<<el;
-    }
-    
-    for(int i = 1;i<=n;i++){
-        for(int j = 1;j<=m;j++) cout<<du[i][j]<<" ";
-        cout<<el;
-    }
-        
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 1; j < m;j++){
+    //         int t = max(a[i][j], a[i][j + 1]) - min(a[i][j], a[i][j + 1]);
+    //         cout<<t<<" ";
+    //         if(!du[i][j]) sum += t;
+    //     }
+    //     cout<<el;
+    // }
     cout<<sum<<el;
     return 0;
 }
--- HSG_competition/High school (THPT)/Tree/CENTROID-OF-TREE.cpp
+++ HSG_competition/High school (THPT)/Tree/CENTROID-OF-TREE.cpp
@@ -3,17 +3,17 @@ using namespace std;
 #define el "\n"
 
 int n;
-const int maxn = (int)1e5 + 5;
+const int maxn = 2 * (int)1e5 + 5;
 vector<vector<int>> a(maxn);
 int subsz[maxn];
 
-int getSubsz(int u, int parent){
+int getSubSz(int u, int parent){
     int &sz = subsz[u];
     sz = 1;
 
     for(int v : a[u]){
         if(v == parent) continue;
-        sz += getSubsz(v, u);
+        sz += getSubSz(v, u);
     }
     return sz;
 }
@@ -21,29 +21,25 @@ int getSubsz(int u, int parent){
 int getCen(int u, int parent){
     for(int v : a[u]){
         if(v == parent) continue;
-
         if(subsz[v] * 2 > n) return getCen(v, u);
     }
     return u;
 }
 
 int main(){
-    freopen(".\\txt\\GIAM-SAT-VUONG-QUOC.INP", "r", stdin);
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
     cin>>n;
-    for(int i =1;i<n;i++){
+    for(int i = 1; i < n;i++){
         int u, v;
         cin>>u>>v;
         a[u].push_back(v);
         a[v].push_back(u);
     }
 
-    getSubsz(1, 0);
-    
-    for(int i=1;i<=n;i++) cout<<subsz[i]<<" "; cout<<el;
+    getSubSz(1, 0);
     cout<<getCen(1, 0)<<el;
 
     return 0;
