213e9af7a01b7b26814864645ac81300d296de7d
HnpawsPham <hanpham123th@gmail.com>
2024-11-11 23:31:56 +0700

    v85.5

--- HSG_competition/High school (THPT)/Tree/CENTROID-OF-TREE2.cpp
+++ HSG_competition/High school (THPT)/Tree/CENTROID-OF-TREE2.cpp
@@ -4,30 +4,65 @@ using namespace std;
 
 int n;
 const int maxn = (int)1e5 + 5;
-vector<vector<int>> a(maxn);
+vector<vector<int>> a(maxn), cenTree(maxn);
 bool check[maxn], isCen[maxn];
 int subsz[maxn];
 
-void dfs(int u, int cnt){
+int getSz(int u){
+    int sz = 1;
     check[u] = 1;
     subsz[u] = 1;
 
     for(int v : a[u]){
         if(check[v] || isCen[v]) continue;
-        dfs(v, cnt + 1);
+        n += getSz(v);
+        getSz(v);
         subsz[u] += subsz[v];
     }
-    return;
+    return sz;
+}
+
+int initCen(int u, int n){
+    bool ok = 1;
+    check[u] = 1;
+    int maxx = 0;
+
+    for(int v : a[u]){
+        if(check[v] || isCen[v]) continue;
+
+        if(subsz[v] > n / 2) ok = 0;
+        if(maxx == 0 || subsz[v] > subsz[maxx]) maxx = v;
+    }
+
+    if(ok && n - subsz[u] <= n / 2) return u;
+    return initCen(maxx, n);
 }
 
 int getCen(int u){
-    memset(check, 0, sizeof(check));
     memset(subsz, 0, sizeof(subsz));
 
-    int cen = getCen(u);
-    isCen[cen] = 1;
+    int n = 0;
+    getSz(u);
+    memset(check, 0, sizeof(check));
 
+    int cen = initCen(u, n);
+    isCen[cen] = 1;
     return cen;
+}
+
+int dcpTree(int u){
+    int cur = getCen(u);
+    cout<<cur<<el;
+
+    for(int v : a[cur]){
+        if(isCen[v]) continue;
+
+        int cenSub = dcpTree(v);
+
+        cenTree[cur].push_back(cenSub);
+        cenTree[cenSub].push_back(cur);
+    }
+    return cur;
 }
 
 int main(){
