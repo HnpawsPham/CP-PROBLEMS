32b66adea1bf0ae3a6a081c89315802235f446f3
HnpawsPham <hanpham123th@gmail.com>
2025-02-25 23:28:20 +0700

    v113

--- HSG_competition/High school (THPT)/Dynamic_(QHĐ)/KNAPSACK.cpp
+++ HSG_competition/High school (THPT)/Dynamic_(QHĐ)/KNAPSACK.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int n, s;
 
-int main()
-{
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
@@ -13,27 +12,16 @@ int main()
     vector<int> w(n + 1), v(n + 1);
     vector<vector<int>> dp(n + 1, vector<int>(s + 1));
 
+    for (int i = 1; i <= n; i++) cin>>w[i];
+    for (int i = 1; i <= n; i++) cin>>v[i];
+    
     for (int i = 1; i <= n; i++)
-    {
-        cin>>w[i];
-    }
-
-    for (int i = 1; i <= n; i++)
-    {
-        cin>>v[i];
-    }
-
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= s; j++)
-        {
+        for (int j = 1; j <= s; j++){
             dp[i][j] = dp[i - 1][j];
 
-            if(w[i] <= j){
+            if(w[i] <= j)
                 dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
-            }
         }
-    }
 
     cout << dp[n][s] << endl;
     return 0;
