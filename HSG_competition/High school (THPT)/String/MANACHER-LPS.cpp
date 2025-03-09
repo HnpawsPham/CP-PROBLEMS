a2f139e5276bd0e080428e64bb31a666d96e88cc
HnpawsPham <hanpham123th@gmail.com>
2025-03-09 16:31:30 +0700

    v119

--- HSG_competition/High school (THPT)/String/MANACHER-LPS.cpp
+++ HSG_competition/High school (THPT)/String/MANACHER-LPS.cpp
@@ -1,40 +1,43 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-string str;
-vector<vector<int>> dp(3003, vector<int>(3003, 0));
+#define el "\n"
 
-int main()
-{
-    ios::sync_with_stdio(false);
+int n;
+const int maxn = 100005;
+int p[maxn] = {};  
+char s[maxn]; 
+
+int main() {
+    ios::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
-
-    cin >> str;
-    string s = "|";
-
-    for (auto x : str)
-        s = s + x + "|";
     
-    int n = s.length(), r = 0, c = 0, res = 0;
-    vector<int> p(n, 0);
+    cin >> n;
 
-    for (int i = 1; i < n; i++){
-        if (i < r)
-            p[i] = min(r - i, p[2 * c - i]);
-        
-        while (i - p[i] >= 0 && i + p[i] < n && s[i - p[i]] == s[i + p[i]])
+    s[0] = '|';
+    for(int i = 0; i < n; i++) {
+        char c; cin >> c;
+        s[2 * i + 1] = c;
+        s[2 * i + 2] = '|';
+    }
+
+    n = 2 * n + 1;
+    
+    int r = 0, c = 0, res = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) 
+            p[i] = min(p[2 * c - i], r - i);
+
+        while(i + p[i] < n && s[i + p[i]] == s[i - p[i]]) 
             p[i]++;
-        
-        if (i + p[i] >= r){
+
+        if(i + p[i] > r) {
             r = i + p[i];
             c = i;
         }
-
         res = max(res, p[i] - 1);
     }
 
-    cout << res << endl;
-
+    cout << res;
     return 0;
 }
