ccd373cd7fa5b4d0ca21ab9c7e00d4d89105ee72
HnpawsPham <hanpham123th@gmail.com>
2025-03-19 23:02:39 +0700

    v126

--- IT_Squad_Journey/THPT/Fenwick/CAP-NGHICH-THE.cpp
+++ IT_Squad_Journey/THPT/Fenwick/CAP-NGHICH-THE.cpp
@@ -5,30 +5,25 @@ using ll = long long;
 int n;
 vector<int> t(1e5 + 1, 0);
 
-void update(int i, int del)
-{
-    while (i > 0)
-    {
+void update(int i, int del){
+    while (i > 0){
         t[i] += del;
         i -= (i & -i);
     }
+    return;
 }
 
-int sum(int i)
-{
+int sum(int i){
     int res = 0;
 
-    while (i <= n)
-    {
+    while (i <= n){
         res += t[i];
         i += (i & -i);
     }
-
     return res;
 }
 
-int main()
-{
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -36,16 +31,13 @@ int main()
     cin >> n;
     ll ans = 0;
 
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++){
         int x;
         cin >> x;
 
         update(x, 1);
         ans += sum(x + 1);
     }
-
     cout<<ans<<endl;
-
     return 0;
 }
