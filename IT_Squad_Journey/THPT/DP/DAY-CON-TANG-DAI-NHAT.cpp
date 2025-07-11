1c72eabd9d704099e092680ab700ec09a5b8f1db
HnpawsPham <hanpham123th@gmail.com>
2025-07-11 20:52:08 +0700

    v222

--- IT_Squad_Journey/THPT/DP/DAY-CON-TANG-DAI-NHAT.cpp
+++ IT_Squad_Journey/THPT/DP/DAY-CON-TANG-DAI-NHAT.cpp
@@ -4,8 +4,7 @@ using ll = long long;
 
 int n;
 
-int main()
-{
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
@@ -13,26 +12,18 @@ int main()
     cin >> n;
     vector<ll> a(n), cnt(n, 1);
 
-    for (auto &x : a)
-    {
-        cin >> x;
-    }
+    for (auto &x : a) cin >> x;
 
     ll maxlen = 0;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < i; j++){
             if (a[i] > a[j])
-            {
                 cnt[i] = max(cnt[i], cnt[j] + 1);
-            }
         }
-
         maxlen = max(cnt[i], maxlen);
     }
 
-    cout << maxlen << endl;
+    cout << maxlen;
     return 0;
 }
